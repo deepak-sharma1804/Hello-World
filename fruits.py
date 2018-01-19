520ccec18b33ec8d125c810bc26a50e6bdbf1e11
Deepak <deepak.dit2009@gmail.com>
2018-01-18 20:28:33 -0600

    Second commit

--- fruits.py
+++ fruits.py
@@ -1,7 +1,18 @@
-fruits = ["APPLE","ORANGE","MANGO","KIWI","BLUEBERRY"]
-user_choce = input("Enter you choice of fruit")
+def guess_fruit(basket):
+    counter = 0
+    while counter < 5:
+        user_choce = input("Enter you choie of fruit ")
+        if user_choce.strip().upper() in basket:
+            print("Your guess is right")
+        else:
+            counter = counter + 1
+            print(counter)
+            if counter == 5:
+                print("you ran out of options")
 
-if user_choce.strip().upper() in fruits:
-    print("Your guess is right")
-else:
-    print("You missed")
+
+def main():
+    fruits = ["APPLE","ORANGE","MANGO","KIWI","BLUEBERRY"]
+    guess_fruit(fruits)
+
+main()
